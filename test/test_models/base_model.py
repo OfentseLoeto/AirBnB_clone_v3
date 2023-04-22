baf636b6dc7c69a28eef483105e4a84aa97cfaa1
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-04-22 10:00:39 -0700

    task created

---
+++ test/test_models/base_model.py
@@ -0,0 +1,44 @@
+import models
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+
+            for key, value in kwargs.items():
+
+                if key == "created_at":
+                    self.created_at = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4)
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+    def __str__(self)
+
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+
+        dic = self.__dict__.copy()
+        dic["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dic["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dic["__class__"] = self.__class__.__name__
+        return dic
