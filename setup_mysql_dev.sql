33804b2df485aa3fb69f97fc533b2ced98bf952a
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-20 09:25:04 -0700

    Task 4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,23 +2,33 @@
 
 import MySQLdb
 
-conn = MySQLdb.connect(host='localhost', user=hbnb_test, passwd=hbnb_test_passwd)
+'''Connect to the MySQL server as'''
 
+conn = MySQLdb.connect(host='localhost', user="hbnb_test", passwd="hbnb_test_pwd")
+
+'''Create a cursor object to execute SQL queries'''
 cursor = conn.cursor()
+
+'''Create the hbnb_test_db database if it doesnt exist'''
 db_query = "CREATE DATABASE IF NOT EXISTS hbnb_test_db"
 cursor.execute(db_query)
 
+'''Create the hbnb_test user with the specified password'''
 user_query = "CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_passwd'"
 cursor.execute(user_query)
 
-grant_privileges = "GRANT ALL PRIVILEGES hbnb_test_db.* TO 'hbnb_test'@'localhost'"
+'''Grant all privileges to hbnb_test on the hbnb_test_db database'''
+grant_privileges = "GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost'"
 cursor.execute(grant_privileges)
 
+'''Grant select on performance_schema'''
 grant_select = "GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'"
 cursor.execute(grant_select)
 
-flush_query = "PRIVILEGES"
+'''Flush privileges to apply the changes'''
+flush_query = "FLUSH PRIVILEGES"
 cursor.execute(flush_query)
 
+'''Close the cursor and database connection'''
 cursor.close()
 conn.close()
