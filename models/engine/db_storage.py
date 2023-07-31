e3bf962ae32898504493eeaa6b3ea4cce0b081e7
ofentseloeto <ofentseloeto610@gmail.com>
2023-07-31 11:44:45 -0700

    Update DBStorage and FileStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python3
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import BaseModel, Base
 
 
 class DBStorage:
