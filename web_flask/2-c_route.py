f9e3c93b1330486f31f2e70f16e6a916e941a175
ofentse loeto <ohentseloeto610@gmail.com>
2023-08-03 14:55:34 -0700

    script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+
+def hello_hbnb():
+    """Display 'Hello HBNB!'"""
+    return 'Hello HBNB'
+
+@app.route('/hbnb', strict_slashes=False)
+
+def hbnb():
+    """Display 'HBNB!'"""
+    return 'HBNB'
+
+@app.route('//<text>', strict_slashes=False)
+
+def c_text(text):
+    """Display 'C' followed by the value of textvariable
+       (replace underscore sysmbol with a space)
+    """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
